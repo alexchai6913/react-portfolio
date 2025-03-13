921a70460e8544584dcb1aedee0141be40c06929
Alex Chai <alexchai@xcalex.com>
2025-03-14 06:59:15 +0800

    Navigation Link Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+
+const Hero = ({ props }) => {
+    return (
+        <div className="Hero">
+            <Navbar />
+        </div>
+    );
+};
+
+export default Hero;
